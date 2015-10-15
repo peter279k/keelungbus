19bce194eb045141875604126ba861f84df0aa91
peter279k <peter279k@gmail.com>
2015-10-15 18:36:19 +0800

    修改非同步站名排序以及使用closure方式傳入參數

--- js/dynamic.js
+++ js/dynamic.js
@@ -1,4 +1,5 @@
 var inter_val = "";
+var href_arr = new Array();
 $(function() {
 	var res = "";
 	show_bus_list();
@@ -9,6 +10,7 @@ $(function() {
 		clearInterval(inter_val);
 		show_bus_list();
 	});
+	
 });
 
 function show_bus_list() {
@@ -34,7 +36,7 @@ function show_bus_list() {
 				var temp = '<div class="col-xs-12 col-sm-6 col-md-4"><div class="box">';
 				var count = 0;
 				for(var res_count=2;res_count<res.length;res_count++) {
-					temp += '<button onclick="get_dynamic_info('+ "'" +res[res_count]["href"] + "'" +')" class="ui-btn"><i class="zmdi zmdi-directions-bus ui-pull-left"></i>'+res[res_count]["content"]+'</button>';
+					temp += '<button onclick="get_dynamic_info('+ "'" +res[res_count]["href"] + "'" + "," + "'" + res[res_count]["content"] + "'" +')" class="ui-btn"><i class="zmdi zmdi-directions-bus ui-pull-left"></i>'+res[res_count]["content"]+'</button>';
 					count += 1;
 					if(count % 6 == 0) {
 						temp += "</div></div>";
@@ -52,7 +54,7 @@ function show_bus_list() {
 	});
 }
 
-function get_dynamic_info(link) {
+function get_dynamic_info(link, link_content) {
 	//即時公車資料(real-time)
 	//use ajax sync
 	$.ajaxSetup({
@@ -73,11 +75,11 @@ function get_dynamic_info(link) {
 			},
 			color : "lime" // optional color of the button (default: 'lime')
 		},
-		ttl : 20000 // optional, time-to-live in ms (default: 3000)
+		ttl : 3000 // optional, time-to-live in ms (default: 3000)
 	});
 	
-	var href_arr = new Array();
-	$("#dynamic-list").append('<li data-role="list-divider">即時公車動態</li>');
+	href_arr = new Array();
+	$("#dynamic-list").append('<li data-role="list-divider">即時公車動態--' + link_content + '</li>');
 	var temp_arr = link.split("=");
 	$.getJSON("../php/get_bus_list.php?action=" + temp_arr[1], function(response) {
 		res = response["query"]["results"]["a"];
@@ -85,18 +87,24 @@ function get_dynamic_info(link) {
 			if(res_count % 2 == 1)
 				continue;
 			href_arr.push(res[res_count]["href"]);
+			var temp = '<li class="ui-li-static ui-body-inherit">';
+			temp += "<h2 id='"+res[res_count]["href"].replace(".", "").replace("&", "").replace("?", "").replace(/=/g, "")+"'>" + res[res_count]["content"] + "</h2>";
+			temp += "</li>";
+			$("#dynamic-list").append(temp);
 		}
+		$("#dynamic-list").listview("refresh");
 	});
 
-	inter_val = setInterval(dynamic_info(href_arr), 60000);
+	inter_val = setInterval(dynamic_info(href_arr), 8000);
 }
 
 function dynamic_info(href_arr) {
+	console.log(href_arr);
 	//toast
 	$.ajaxSetup({
 		async: true
 	});
-	$("#dynamic-list").html('<li data-role="list-divider">即時公車動態</li>');
+	$("h3").remove(".clr-deep-orange");
 		new $.nd2Toast( {
 			// The 'new' keyword is important, otherwise you would overwrite the current toast instance
 			message : "載入資料中...", // Required
@@ -110,40 +118,23 @@ function dynamic_info(href_arr) {
 			},
 			ttl : 3000 // optional, time-to-live in ms (default: 3000)
 		});
-	
-		var temp_count_arr = new Array();
-		var temp_str_arr = new Array();
-		var temp_str = "";
+
 		for(var res_count=0;res_count<href_arr.length;res_count++) {
-			$.getJSON("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%20%3D%20%22http%3A%2F%2F117.56.232.115%2FKLBusWeb%2Fpda%2F" + encodeURIComponent(href_arr[res_count]) + "%22%20and%20xpath%20%3D%20%22%2F%2Ftd%22&format=json&diagnostics=true&callback=", function(response) {
-				var temp = '<li class="ui-li-static ui-body-inherit">';
-				res = response["query"]["results"]["td"];
-				temp += "<h2>" + res[1].trim().replace(/\r/g, "").replace(/\n/g, "") + "</h2>";
-				temp += "<h3 class='clr-deep-orange'>" + res[2]["content"] + "</h3>";
-				temp += "</li>";
-				temp_count_arr.push(res_count);
-				temp_str_arr.push(temp);
-				if(href_arr.length === temp_count_arr.length) {
-					sort_str(temp_count_arr, temp_str_arr);
-				}
-			});
+			$.getJSON("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%20%3D%20%22http%3A%2F%2F117.56.232.115%2FKLBusWeb%2Fpda%2F" + encodeURIComponent(href_arr[res_count]) + "%22%20and%20xpath%20%3D%20%22%2F%2Ftd%22&format=json&diagnostics=true&callback=", 
+				(function(dy_info) {
+					return function(response) {
+						handle_dy_info(response, dy_info);
+						$("#dynamic-list").listview("refresh");
+						// create a new closure on the parameter dy_info
+						// which will hold the correct value at invocation time
+					};
+				}(href_arr[res_count])) // calling the function with the current value
+			);
 		}
 }
 
-function sort_str(temp_count_arr, temp_str_arr) {
-	for(var res_count=0;res_count<temp_count_arr.length;res_count++) {
-		for(var res_count2=res_count+1;res_count2<temp_count_arr.length;res_count2++) {
-			if(temp_count_arr[res_count] > temp_count_arr[res_count2]) {
-				var temp = temp_count_arr[res_count];
-				temp_count_arr[res_count] = temp_count_arr[res_count2];
-				temp_count_arr[res_count2] = temp;
-				temp = temp_str_arr[res_count];
-				temp_str_arr[res_count] = temp_str_arr[res_count2];
-				temp_str_arr[res_count2] = temp;
-			}
-		}
-	}
-
-	$("#dynamic-list").append(temp_str_arr.toString().replace(/,/g , ""));
-	$("#dynamic-list").listview("refresh");
+function handle_dy_info(response, dy_info) {
+	res = response["query"]["results"]["td"];
+	var temp = "<h3 class='clr-deep-orange'>--" + res[2]["content"] + "</h3>";
+	$("#" + dy_info.replace(".", "").replace("&", "").replace("?", "").replace(/=/g, "")).append(temp);
 }
